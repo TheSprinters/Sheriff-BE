ac280ba24dab31282ee161f1d49b947e5579dc9e
MoizL163 <moiz.lukmani@gmail.com>
2026-03-31 12:33:34 -0700

    new u2 table route

--- main.py
+++ main.py
@@ -35,6 +35,10 @@ def index():
 def sheriff_portal():
     return render_template("index.html")
 
+@app.route('/u2table')
+def u2table():
+    return "u2table page"
+
 
 @app.errorhandler(404)
 def page_not_found(e):
