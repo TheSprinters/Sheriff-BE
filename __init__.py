2e29c88725d3654c1161f4564f8bc083f4699186
MoizL163 <moiz.lukmani@gmail.com>
2026-03-31 16:38:12 -0700

    changed ports again

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ load_dotenv()
 app = Flask(__name__)
 
 # Configure Flask Port, default to 8587 which is same as Docker setup
-app.config['FLASK_PORT'] = int(os.environ.get('FLASK_PORT') or 8587)
+app.config['FLASK_PORT'] = int(os.environ.get('FLASK_PORT') or 8325)
 
 # Configure Flask to handle JSON with UTF-8 encoding versus default ASCII
 app.config['JSON_AS_ASCII'] = False
@@ -35,6 +35,7 @@ cors = CORS(
        'http://127.0.0.1:4000',
        'https://open-coding-society.github.io',
        'https://pages.opencodingsociety.com',
+       'https://sheriff.opencoding.society.com',
    ],
    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 )
